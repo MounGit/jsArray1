112a81262072093b9c8e1f3897872c9211cd3935
mounGit <a-mouna@live.fr>
2021-05-03 11:45:39 +0200

    exo

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+// - # Exercice 1 JavaScript -> Array
+
+//     - ## Créer une variable qui reçois un array avec 3 éléments -> ex: coding 11, coding 12, coding 13
+
+let tab = ["brol 1", "brol 2", "brol 3"];
+
+
+//     - ##  Afficher les éléments de l'array
+
+console.log(tab);
+
+//     - ## Afficher le nombre d'éléments de l'array
+
+console.log(tab.length);
+
+//     - ## Afficher les éléments de l'array 1 par 1
+
+console.log(tab[0]);
+console.log(tab[1]);
+console.log(tab[2]);
