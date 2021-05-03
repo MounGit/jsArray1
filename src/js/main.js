a8db4f2d1c62f2c66a9bc30db6c1ab7e37dc00cf
mounGit <a-mouna@live.fr>
2021-05-03 12:05:33 +0200

    exo2

--- src/js/main.js
+++ src/js/main.js
@@ -18,3 +18,19 @@ console.log(tab.length);
 console.log(tab[0]);
 console.log(tab[1]);
 console.log(tab[2]);
+
+
+
+
+
+// - # Exercice 2 JavaScript -> Array
+
+//     - ## Créer une variable qui reçois un array avec 3 éléments -> ex: coding 11, coding 12, coding 13
+let tab2 = ["brol1", "brol2", "brol3"];
+
+//     - ## Afficher les éléments de l'array
+console.log(tab2);
+
+//     - ## Ajouter et afficher votre prénom a l'array
+tab2.push("Mouna");
+console.log(tab2);
